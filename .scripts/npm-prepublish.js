4ef176e487304e11ce60300bcdbe407ccb92f368
Muhammed Thanish <mnmtanish@gmail.com>
2016-10-04 11:49:41 +0530

    Refactor prepublish script

--- .scripts/npm-prepublish.js
+++ .scripts/npm-prepublish.js
@@ -1,9 +1,3 @@
-var path = require('path');
-var shell = require('shelljs');
-var babel = ['node_modules', '.bin', 'babel'].join(path.sep);
-
-// required for react-app preset
-process.env.NODE_ENV = 'production';
-
-shell.rm('-rf', 'dist')
-shell.exec(babel + ' --ignore __tests__ src --out-dir dist')
+const sh = require('shelljs');
+sh.rm('-rf', 'dist');
+sh.exec('babel src --ignore __tests__ --out-dir dist');
